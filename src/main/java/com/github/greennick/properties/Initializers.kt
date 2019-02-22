e5abbf65a87f963f2eab190c68e1dc2c08e7216c
green-nick <paul.grynyk@gmail.com>
2019-02-23 00:16:12 +0200

    - create Property represented Boolean primitive;

--- src/main/java/com/github/greennick/properties/Initializers.kt
+++ src/main/java/com/github/greennick/properties/Initializers.kt
@@ -2,6 +2,8 @@ package com.github.greennick.properties
 
 import com.github.greennick.properties.generic.MutableProperty
 import com.github.greennick.properties.generic.PropertyImpl
+import com.github.greennick.properties.primitives.booleans.BooleanPropertyImpl
+import com.github.greennick.properties.primitives.booleans.MutableBooleanProperty
 import com.github.greennick.properties.primitives.doubles.DoublePropertyImpl
 import com.github.greennick.properties.primitives.doubles.MutableDoubleProperty
 import com.github.greennick.properties.primitives.ints.IntPropertyImpl
@@ -14,3 +16,5 @@ fun <T> emptyProperty(): MutableProperty<T?> = PropertyImpl(null)
 fun propertyOf(value: Int): MutableIntProperty = IntPropertyImpl(value)
 
 fun propertyOf(value: Double): MutableDoubleProperty = DoublePropertyImpl(value)
+
+fun propertyOf(value: Boolean): MutableBooleanProperty = BooleanPropertyImpl(value)
