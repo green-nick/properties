d1b1aa57e13dcadca1afedaa0eb2071231270768
green-nick <paul.grynyk@gmail.com>
2019-02-23 00:37:23 +0200

    - create Property represented Long primitive;

--- src/main/java/com/github/greennick/properties/Initializers.kt
+++ src/main/java/com/github/greennick/properties/Initializers.kt
@@ -8,12 +8,16 @@ import com.github.greennick.properties.primitives.doubles.DoublePropertyImpl
 import com.github.greennick.properties.primitives.doubles.MutableDoubleProperty
 import com.github.greennick.properties.primitives.ints.IntPropertyImpl
 import com.github.greennick.properties.primitives.ints.MutableIntProperty
+import com.github.greennick.properties.primitives.longs.LongPropertyImpl
+import com.github.greennick.properties.primitives.longs.MutableLongProperty
 
 fun <T> propertyOf(value: T): MutableProperty<T> = PropertyImpl(value)
 
 fun <T> emptyProperty(): MutableProperty<T?> = PropertyImpl(null)
 
 fun propertyOf(value: Int): MutableIntProperty = IntPropertyImpl(value)
+
+fun propertyOf(value: Long): MutableLongProperty = LongPropertyImpl(value)
 
 fun propertyOf(value: Double): MutableDoubleProperty = DoublePropertyImpl(value)
 
