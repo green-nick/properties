748543b4a5e76ef62a79b2f8c5fa68efe15404e5
green-nick <paul.grynyk@gmail.com>
2019-04-12 13:31:01 +0300

    - add union function for properties;  - add mapper function for properties;

---
+++ src/main/java/com/github/greennick/properties/generic/PropertiesExt.kt
@@ -0,0 +1,22 @@
+@file:Suppress("unused")
+
+package com.github.greennick.properties.generic
+
+import com.github.greennick.properties.propertyOf
+
+operator fun <T, R> Property<T>.plus(another: Property<R>): Property<Pair<T, R>> {
+    val new = propertyOf(this.value to another.value)
+
+    this.subscribe { new.value = it to another.value }
+    another.subscribe { new.value = this.value to it }
+
+    return new
+}
+
+fun <T, R> Property<T>.map(mapper: (T) -> R): Property<R> {
+    val new = propertyOf(mapper(this.value))
+
+    this.subscribe { new.value = mapper(it) }
+
+    return new
+}
